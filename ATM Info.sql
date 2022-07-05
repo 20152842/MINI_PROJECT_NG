c9a8b7b3337d27f1f6a1681be4d741008a8d9f00
20152842 <20152842@kookmin.ac.kr>
2022-07-05 16:41:09 +0900

    database 16:39

--- ATM Info.sql
+++ ATM Info.sql
@@ -1,15 +1,21 @@
 create database ATM_db default character set = utf8mb4;
 use ATM_db;
-drop table borrower; # 테이블삭제
+drop table customer; # 테이블삭제
+truncate loan; # 테이블 리셋
 show tables;
 
-create table account(
-	account_name varchar(500) null,
-    account_id varchar(100) not null,
-    account_desc varbinary(500),
-    constraint PK_ACCOUNT primary key(account_id)
+create table accounts(
+	accounts_name varchar(100),
+    accounts_id varchar(100) not null,
+    accounts_desc varbinary(100),
+    customer_id varchar(100) not null,
+    constraint PK_ACCOUNTS primary key(accounts_id)
 )engine = InnoDB default character set = utf8mb4;
-truncate loan; # 테이블 리셋
+
+alter table accounts
+	add constraint FK_ACCOUNTS foreign key(customer_id) references customer (customer_id);
+
+desc accounts;
 
 INSERT INTO account VALUES('Downrown', 'A-101', 500);
 INSERT INTO account VALUES('Mianus', 'A-102', 700);
@@ -20,16 +26,31 @@ INSERT INTO account VALUES('Redwood', 'A-106', 700);
 INSERT INTO account VALUES('Brighton', 'A-107', 750);
 
 select *
-from account;
+from accounts;
 
 create table customer(#고객
-	customer_id varchar(20) not null,
-	customer_name varchar(20),
-	customer_street varchar(20),
-	customer_city varchar(20),
+	customer_id varchar(100) not null,
+	customer_name varchar(100),
+	customer_street varchar(100),
+	customer_city varchar(100),
+    accounts_id varchar(100),
+    depositor_id varchar(100),
+    branch_id varchar(100),
+    city_id varchar(100),
     constraint PK_CUSTOMER primary key(customer_id)
 )engine = InnoDB default character set = utf8mb4;
-
+alter table customer
+	rename column depositer_id to depositor_id;
+ desc customer;   
+alter table customer
+	add constraint FK_CUSTOMER foreign key(accounts_id) references accounts (accounts_id);
+alter table customer
+	add constraint FK_CUSTOMER_2 foreign key(depositor_id) references depositor (depositor_id);
+alter table customer
+	add constraint FK_CUSTOMER_3 foreign key(branch_id) references branch (branch_id);
+alter table customer
+	add constraint FK_CUSTOMER_4 foreign key(city_id) references city (city_id);
+    
 INSERT INTO customer VALUES('C-101','Jones', 'Main', 'harrison');
 INSERT INTO customer VALUES('C-102','Smith', 'North', 'Rye');
 INSERT INTO customer VALUES('C-103','Hayes', 'Main', 'Harrison');
@@ -44,12 +65,26 @@ INSERT INTO customer VALUES('C-111','Brooks', 'Senator', 'Brooklyn');
 INSERT INTO customer VALUES('C-112','Girccn', 'Walnut', 'Stamford');
 
 CREATE TABLE branch(#지점
-	branch_id varchar(20) not null,
-	branch_name varchar(20) not null,
-	branch_city varchar(20),
-	asserts varbinary(10),
+	branch_id varchar(100) not null,
+	branch_name varchar(100) not null,
+	branch_city varchar(100),
+	branch_asserts varbinary(100),
+    customer_id varchar(100),
+    city_id varchar(100),
+    depositor_id varchar(100),
 	constraint PK_BRANCH primary key(branch_id)
 )engine = InnoDB default character set = utf8mb4;
+
+alter table branch
+	rename column depositer_id to depositor_id;
+
+alter table branch
+	add constraint FK_BRANCH foreign key(customer_id) references customer (customer_id);
+alter table branch
+	add constraint FK_BRANCH_2 foreign key(city_id) references city (city_id);
+alter table branch
+	add constraint FK_BRANCH_3 foreign key(depositor_id) references depositor (depositor_id);
+
 
 INSERT INTO branch VALUES('B-101','Downtown', 'Brooklyn', 9000000);
 INSERT INTO branch VALUES('B-102','Redwood', 'Palo Alto', 2100000);
@@ -59,34 +94,25 @@ INSERT INTO branch VALUES('B-105','Round Hill', 'Horsencck', 8000000);
 INSERT INTO branch VALUES('B-106','Pownal', 'Bennington', 300000);
 INSERT INTO branch VALUES('B-107','North Town', 'Rye', 3700000);
 INSERT INTO branch VALUES('B-108','Brighton', 'Brooklyn', 7100000);
+#
 
-
-CREATE TABLE loan(
-	loan_id varchar(20),
-    loan_name varchar(20),
-	loan_number varchar(20),
-	loan_cost varbinary(10),
-    constraint PK_LOAN primary key(loan_id)
-    )engine = InnoDB default character set = utf8mb4;
-    
-INSERT INTO loan VALUES('L-101', 'Downtown', 'L-17', 1000);
-INSERT INTO loan VALUES('L-102','Redwood', 'L-23', 2000);
-INSERT INTO loan VALUES('L-103','Perryridge', 'L-15', 1500);
-INSERT INTO loan VALUES('L-104','Downtown', 'L-14', 1500);
-INSERT INTO loan VALUES('L-105','Mianus', 'L-93', 500);
-INSERT INTO loan VALUES('L-106','Round Hill', 'L-11', 900);
-INSERT INTO loan VALUES('L-107','Perryridge', 'L-16', 1300);
-
-select *
-from loan;
 CREATE TABLE depositor(
-	depositor_id CHAR(20),
-    depositor_name varchar(20),
-	account_number CHAR(20),
+	depositor_id varCHAR(100),
+    depositor_name varchar(100),
+	accounts_number varCHAR(100),
+    customer_id varchar(100),
+    city_id varchar(100),
+    branch_id varchar(100),
 	constraint PK_DEPOSITOR primary key(depositor_id)
     )engine = InnoDB default character set = utf8mb4;
     
-
+alter table depositor
+	add constraint FK_DEPOSITOR foreign key(customer_id) references customer (customer_id);  
+alter table depositor
+	add constraint FK_DEPOSITOR_2 foreign key(city_id) references city (city_id);
+alter table depositor
+	add constraint FK_DEPOSITOR_3 foreign key(branch_id) references branch (branch_id);
+    
 INSERT INTO depositor VALUES('d-101','Johnson', 'A-101');
 INSERT INTO depositor VALUES('d-102','Smith', 'A-215');
 INSERT INTO depositor VALUES('d-103','Hayes', 'A-102');
@@ -95,16 +121,23 @@ INSERT INTO depositor VALUES('d-105','Jason', 'A-201');
 INSERT INTO depositor VALUES('d-106','Jones', 'A-217');
 INSERT INTO depositor VALUES('d-107','Lindsay', 'A-222');
 
-CREATE TABLE borrower(
-	customer_name varchar(20),
-	loan_number varchar(20),
-    constraint PK_BORROWER primary key(customer_name)
-	)engine = InnoDB default character set = utf8mb4;
-    
-INSERT INTO borrower VALUES('Johnson', 'A-101');
-INSERT INTO borrower VALUES('Smith', 'A-215');
-INSERT INTO borrower VALUES('Hayes', 'A-102');
-INSERT INTO borrower VALUES('Turner', 'A-305');
-INSERT INTO borrower VALUES('Jason', 'A-201');
-INSERT INTO borrower VALUES('Jones', 'A-217');
-INSERT INTO borrower VALUES('Lindsay', 'A-222');
+CREATE TABLE city(
+	city_id varchar(100),
+	city_street varchar(100),
+    city_city varchar(100),
+    customer_id varchar(100),
+    branch_id varchar(100),
+    constraint PK_CITY primary key(city_id)
+)engine = InnoDB default character set = utf8mb4;
+
+alter table city
+	add constraint FK_CITY foreign key(customer_id) references customer (customer_id);
+alter table city
+	add constraint FK_CITY_2 foreign key(branch_id) references branch (branch_id);
+
+show tables;
+desc city;
+desc branch;
+desc accounts;
+desc customer;
+desc depositor;
